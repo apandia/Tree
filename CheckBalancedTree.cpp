506d7273bc752ab2014da3df3ac7e73ad7a12a2d
apandia <96688353+apandia@users.noreply.github.com>
2022-05-11 16:41:29 +0530

    Update CheckBalancedTree.cpp

--- CheckBalancedTree.cpp
+++ CheckBalancedTree.cpp
@@ -1,171 +1,84 @@
 #include<iostream>
-#include<queue>
+#include<stack>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Node {
 
 public:
-	int data;
-	Node* left;
-	Node* right;
+    int data;
+    Node* left;
+    Node* right;
 
-	Node(int d) {
-		data = d;
-		left = nullptr;
-		right = nullptr;
-	}
+    Node(int d) {
+        data = d;
+        left = nullptr;
+        right = nullptr;
+    }
 };
 
-/*Tree
-	1
-  /   \
- 2	   3
-	 /   \
-	4	  6
-   /
-  5
+/*Binary Tree Traversal
+Input:
+      1
+    /   \
+   2     3
+  / \   / \
+ 4   5 6   7
+          / \
+         8   9
 */
 
-/*Level Order Traversal Input
-1
-2 3
--1 -1
-4 6
-5 -1
--1 -1
--1 -1
-*/
-Node* buildTree()
-{
-
-	int d;
-	cin >> d;
-
-	Node* root = new Node(d);
-	queue<Node*> q;
-	q.push(root);
-
-	while (!q.empty()) {
-		Node* current = q.front();
-		q.pop();
-
-		int c1, c2;
-		cin >> c1 >> c2;
-
-		if (c1 != -1) {
-			current->left = new Node(c1);
-			q.push(current->left);
-		}
-		if (c2 != -1) {
-			current->right = new Node(c2);
-			q.push(current->right);
-		}
-	}
-
-	return root;
-}
-
-
-/*Level Order Traversal Expected Output
-1
-2 3
-4 6
-5
-*/
-vector<vector<int>> levelOrder(Node *root)
-{
-	vector<vector<int>> ans;
-
-	if (root == nullptr)
-		return ans;
-
-	queue<Node*> q;
-	q.push(root);
-
-	while (!q.empty())
-	{
-		int size = q.size();
-		vector<int> currLevelData;
-
-		for (int counter = 0; counter < size; counter++)
-		{
-			Node* current = q.front();
-			q.pop();
-
-			if (current->left != nullptr) {
-				q.push(current->left);
-			}
-
-			if (current->right != nullptr) {
-				q.push(current->right);
-			}
-
-			currLevelData.push_back(current->data);
-			cout << current->data << " ";
-		}
-
-		cout << endl;
-
-		ans.push_back(currLevelData);
-	}
-
-	return ans;
-}
-
+//Output : 4
+/** Complexity:
+  * TC: O(N)
+  * SC: O(N) for skewed tree
+ **/
 int getTreeHeight(Node* root)
 {
-	if (root == nullptr)
-		return 0;
+    if(root == nullptr)
+        return 0;
 
-	int lh = getTreeHeight(root->left);
-	if (lh == -1) {
-		return -1;
-	}
-	int rh = getTreeHeight(root->right);
-	if (rh == -1) {
-		return -1;
-	}
+    int leftHeight = getTreeHeight(root->left);
+    if(leftHeight == -1)
+        return -1;
 
-	if (abs(lh - rh) > 1)
-	{
-		cout << "Tree is imbalanced at node = " << root->data << endl;
-		cout << "Left height = " << lh << " Right height is = " << rh << endl;
+    int rightHeight = getTreeHeight(root->right);
+    if(rightHeight == -1)
+        return -1;
 
-		return -1;
-	}
+    if(abs(leftHeight - rightHeight) > 1){
+        return -1;
+    }
 
-	return 1 + max(lh, rh);
+    return 1 + max(leftHeight, rightHeight);
 }
 
-bool checkBalancedTree(Node* root)
+bool isBalanced(Node* root)
 {
-	return getTreeHeight(root) != -1;
+    return (getTreeHeight(root) != -1);
 }
 
 //Driver function
-int main() 
+int main()
 {
-	Node* root = buildTree();
+    Node* root = new Node(1);
 
-	cout << "Levelorder Traversal Print: " << endl;
-	vector<vector<int>> lo = levelOrder(root);
+    root->left = new Node(2);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
 
-	for (int i = 0; i < lo.size(); i++)
-	{
-		vector<int> level = lo[i];
-		for (int j = 0; j < level.size(); j++)
-		{
-			cout << level[j] << " ";
-		}
-		cout << endl;
-	}
+    root->right = new Node(3);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    root->right->right->left = new Node(8);
+    root->right->right->right = new Node(9);
 
-	if(checkBalancedTree(root))
-		cout << "Tree is balanced"  << endl;
-	else
-		cout << "Tree is not balanced" << endl;
+    if(isBalanced(root))
+        cout<<"Tree is balanced"<<endl;
+    else
+        cout<<"Tree is not balanced"<<endl;
 
-	return 0;
+    return 0;
 }
