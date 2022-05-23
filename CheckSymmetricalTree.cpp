2197ec847de2228aca21b23b278419bd1306a670
apandia <96688353+apandia@users.noreply.github.com>
2022-05-23 10:37:31 +0530

    Update CheckSymmetricalTree.cpp

--- CheckSymmetricalTree.cpp
+++ CheckSymmetricalTree.cpp
@@ -1,77 +1,104 @@
-#include<iostream>
-#include<queue>
-#include<map>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 
 class Node {
 
 public:
-	int data;
-	Node* left;
-	Node* right;
+    int data;
+    Node* left;
+    Node* right;
 
-	Node(int d) {
-		data = d;
-		left = nullptr;
-		right = nullptr;
-	}
+    Node(int d) {
+        data = d;
+        left = nullptr;
+        right = nullptr;
+    }
 };
 
-/* Complexity:
- * TC - O(N)
- * SC - O(N) (For skewed tree)
- */
-bool traverse(Node* tree1, Node* tree2)
-{
-	if (tree1 == nullptr || tree2 == nullptr)
-		return (tree1 == tree2);
+//Symmetric Binary Tree (symmetric around its center)
+/*Usecase1 Input Tree:
+        1
+      /   \
+     2     2
+    / \   / \
+   3   4 4   3
 
-	return ((tree1->data == tree2->data)
-		&& traverse(tree1->left, tree2->right)
-		&& traverse(tree1->right, tree2->left));
+Output: Tree is Symmetrical
+*/
+
+/*Usecase2 Input Tree:
+        1
+      /   \
+     2     3
+    / \     \
+   4   5     7
+      /
+     6
+
+Output: Tree is not Symmetrical
+*/
+
+//TC: O(N)
+//SC: O(N) (for skewed tree)
+bool traverseTree(Node* root1, Node* root2)
+{
+    if((root1 == nullptr) || (root2 == nullptr))
+        return (root1 == root2);
+
+    return ((root1->data == root2->data)
+            && traverseTree(root1->left, root2->right)
+            && traverseTree(root1->right, root2->left));
 }
 
-bool checkSymmetricalTree(Node* root)
+bool isSymmetricalTree(Node* root)
 {
-	if (root == nullptr)
-	{
-		return true;
-	}
-		
-	return traverse(root->left, root->right);
-}
+    if(root == nullptr)
+        return true;
 
+    return traverseTree(root->left, root->right);
+}
 
 //Driver function
 int main()
 {
-/*
-        1
-      /   \
-	 2     2
-    / \   / \
-   4   5 5   4
+    //Usecase1
+    Node* root1 = new Node(3);
 
-   left and right side are mirror of one another
-*/
+    root1->left = new Node(2);
+    root1->left->left = new Node(3);
+    root1->left->right = new Node(4);
 
-	Node* root = new Node(1);
+    root1->right = new Node(2);
+    root1->right->left = new Node(4);
+    root1->right->right = new Node(3);
 
-	root->left = new Node(2);
-	root->left->left = new Node(4);
-	root->left->right = new Node(5);
+    //Usecase2
+    Node* root2 = new Node(1);
 
-	root->right = new Node(2);
-	root->right->left = new Node(5);
-	root->right->right = new Node(4);	
+    root2->left = new Node(2);
+    root2->left->left = new Node(4);
+    root2->left->right = new Node(5);
+    root2->left->right->left = new Node(6);
 
-	if (checkSymmetricalTree(root)) {
-		cout << "Tree is Symmetrical" << endl;
-	}
-	else {
-		cout << "Tree is Not Symmetrical" << endl;
-	}
+    root2->right = new Node(3);
+    root2->right->right = new Node(7);
 
-	return 0;
+    cout<<"Usecase1: ";
+    if(isSymmetricalTree(root1))
+        cout<< "Tree is Symmetrical"<<endl;
+    else
+        cout<< "Tree is not Symmetrical"<<endl;
+
+    cout<<"Usecase2: ";
+    if(isSymmetricalTree(root2))
+        cout<< "Tree is Symmetrical"<<endl;
+    else
+        cout<< "Tree is not Symmetrical"<<endl;
+
+    return 0;
 }
