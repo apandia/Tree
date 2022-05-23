8e63039b6751f7eaf58fe11573d7dcf37885396f
apandia <96688353+apandia@users.noreply.github.com>
2022-05-23 11:35:18 +0530

    Update RootToNodePath.cpp

--- RootToNodePath.cpp
+++ RootToNodePath.cpp
@@ -1,90 +1,114 @@
-#include<iostream>
-#include<queue>
-#include<map>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Node {
 
 public:
-	int data;
-	Node* left;
-	Node* right;
+    int data;
+    Node* left;
+    Node* right;
 
-	Node(int d) {
-		data = d;
-		left = nullptr;
-		right = nullptr;
-	}
+    Node(int d) {
+        data = d;
+        left = nullptr;
+        right = nullptr;
+    }
 };
 
-/* Complexity:
- * TC - O(N)
- * SC - O(H) (For skewed tree O(N))
- */
-bool traverse(Node* root, vector<int>& path, int value)
+//Root to node path
+/*
+Input Tree:
+         1
+        / \
+       2   3
+          / \
+         4   5
+        / \
+       6   7
+          /
+         8
+
+Output: Path to 8 = 1->3->4->7->8
+Output: Path to 6 = 1->3->4->6
+Output: Path to 9 = Path does not exist
+*/
+
+//TC: O(N)
+//SC: O(H) (O(N) for skewed tree)
+bool traverseTree(Node* curr, vector<int>& result, int val)
 {
-	if (root == nullptr)
-		return false;
+    if(curr == nullptr)
+        return false;
 
-	path.push_back(root->data);
+    //1. Push the node value in vector
+    result.push_back(curr->data);
 
-	if (root->data == value)
-		return true;
+    //2. If node value equals required value return true
+    if(curr->data == val)
+        return true;
 
-	if (traverse(root->left, path, value) || traverse(root->right, path, value)) {
-		return true;
-	}
+    //3. traverse on left and right subtree and return true if any of the subtree return true
+    if(traverseTree(curr->left, result, val) || traverseTree(curr->right, result, val))
+        return true;
 
-	path.pop_back();
-	return false;
+    //4. Expected value is not found in the path. Pop the last element from vector and return false
+    result.pop_back();
+    return false;
 }
 
-void printPath(Node* root, int value)
+vector<int> rootToNodePath(Node* root, int  value)
 {
-	if (root == nullptr)
-	{
-		return;
-	}
+    vector<int> ans;
 
-	vector<int> path;
-		
-	bool pathExist = traverse(root, path, value);
+    bool pathFound = traverseTree(root, ans, value);
 
-	for (auto data : path) {
-		cout << data << " " ;
-	}
+    return ans;
 }
-
 
 //Driver function
 int main()
 {
-/*
-        1
-      /   \
-	 2     3
-    / \   / \
-   4   5 6   7
-    \
-     8
+    Node* root = new Node(1);
 
-   Output: 1 2 4 8 (path to 8)
-*/
+    root->left = new Node(2);
 
-	Node* root = new Node(1);
+    root->right = new Node(3);
+    root->right->left = new Node(4);
+    root->right->right = new Node(5);
 
-	root->left = new Node(2);
-	root->left->left = new Node(4);
-	root->left->right = new Node(5);
-	root->left->left->right = new Node(8);
+    root->right->left->left = new Node(6);
+    root->right->left->right = new Node(7);
 
-	root->right = new Node(3);
-	root->right->left = new Node(6);
-	root->right->right = new Node(7);	
+    root->right->left->right->left = new Node(8);
 
-	cout << "Root to Node path: ";
-	printPath(root, 8);
+    cout<<"Path from root to node 8 is = ";
+    vector<int> path = rootToNodePath(root, 8);
 
-	return 0;
+    for(auto value : path){
+        cout<<value<<" ";
+    }
+    cout<<endl;
+
+    cout<<"Path from root to node 6 is = ";
+    path = rootToNodePath(root, 6);
+
+    for(auto value : path){
+        cout<<value<<" ";
+    }
+    cout<<endl;
+
+    cout<<"Path from root to node 9 is = ";
+    path = rootToNodePath(root, 9);
+
+    if(path.size() == 0)
+        cout<<"Path does not exist"<<endl;
+
+    for(auto value : path){
+        cout<<value<<" ";
+    }
+    cout<<endl;
+
+    return 0;
 }
